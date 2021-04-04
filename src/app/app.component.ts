13afed082c17d8215ab1ca469664bf54d53312b3
siloob <antonin.garanto@ecole.ensicaen.fr>
2021-04-04 16:17:59 +0200

    add navbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+
+export class AppComponent implements OnInit {
+  public currentView: number | undefined;
   title = 'RapData-Ui';
+
+  ngOnInit(): void {
+    this.currentView = 1;
+  }
+
+  // tslint:disable-next-line:typedef
+  public goTo(nextView: number){
+    this.currentView = nextView;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,19 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { WelcomeComponent } from './welcome/welcome.component';
+import { DocumentationComponent } from './documentation/documentation.component';
+import { TarifComponent } from './tarif/tarif.component';
+import { ContactComponent } from './contact/contact.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    WelcomeComponent
+    WelcomeComponent,
+    DocumentationComponent,
+    TarifComponent,
+    ContactComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
